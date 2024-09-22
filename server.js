e663378db1d4d47fce1d353a274701d5162cf99d
Ahmad alzein <ahmad@basma.com>
2024-09-22 23:19:09 +0300

    first commit

--- server.js
+++ server.js
@@ -172,7 +172,7 @@ io.on('connection', (socket) => {
         console.log('A user disconnected:', socket.id);
     });
 });
-
-server.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
     console.log('Server is running on port 3000');
 });
